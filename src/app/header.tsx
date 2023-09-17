2e592ce2ea8a26fbf1f5dfd216a59a714a274e1c
jme-rs <sasaki0hajime@yahoo.co.jp>
2023-09-17 23:03:25 +0900

    first commit

---
+++ src/app/header.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./header.module.css";
+import { useState } from "react";
+import Link from "next/link";
+
+export default function Header() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const links = new Map([
+    ["Home", "/"],
+    ["About", "/about"],
+    ["Example", "/example"],
+  ]);
+
+  const sidebar = (
+    <div className={`${styles.sidebar} ${isSidebarOpen ? styles.open : ''}`}>
+      <div className={styles.linkContainer}>
+        {
+          Array.from(links).map(([name, href]) => (
+            <Link href={href} className={styles.linkItem}>
+              {name}
+            </Link>
+          ))
+        }
+      </div>
+    </div>
+  );
+
+  return (
+    <div>
+      <div className={styles.container}>
+        <button className={styles.toggleButton} onClick={toggleSidebar}>
+          <FontAwesomeIcon icon={faBars} className={styles.color} />
+        </button>
+        <div className={styles.title}>jme - Next.js</div>
+      </div>
+      <div className={`${styles.wrapper} ${isSidebarOpen ? styles.active : ''}`} onClick={toggleSidebar} />
+      {sidebar}
+    </div>
+  );
+}
