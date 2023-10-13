895693db7c1456b088c0f7ba5f5859a0eaf87592
jme-rs <sasaki0hajime@yahoo.co.jp>
2023-10-13 22:45:14 +0900

    deploy test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
+"use client"
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Header } from '@/components/header'
 import Footer from '@/components/footer'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,12 +19,17 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const currentURL = usePathname();
+  const segments = currentURL.split('/');
+  const parentPath = segments.slice(0, segments.length - 1).join('/');
+
   return (
     <html lang="en">
       <body className={inter.className}>
         <Header />
         <div className='wrapper'>
           <main>
+            <Link href={parentPath}>back</Link>
             {children}
           </main>
         </div>
