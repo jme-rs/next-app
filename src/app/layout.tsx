3edb0c160acfb59c302c8d93ca0e509568fc4d39
jme-rs <sasaki0hajime@yahoo.co.jp>
2023-10-24 23:34:54 +0900

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,6 @@ export const metadata: Metadata = {
     }
   ],
   other: {
-    "manifest": "/manifest.json",
     "apple-mobile-web-app-capable": "yes",
     "apple-mobile-web-app-status-bar-style": "default",
   }
