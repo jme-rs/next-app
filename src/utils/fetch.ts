c9ed7eb30ed9e19c2bc8fcc25eb523f1ea8245d3
jme-rs <sasaki0hajime@yahoo.co.jp>
2023-11-06 18:43:06 +0900

    Refactor fetch.ts to include getCodeFromGithub function

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -2,17 +2,17 @@ import path from "path";
 import fs from "fs";
 import { JSDOM } from "jsdom";
 
-// export async function getCodeFromGithub(owner: string, repo: string, path: string) {
-//   const response = await fetch(
-//     "https://api.github.com/repos/" + owner + "/" + repo + "/contents/" + path,
-//     // { cache: "no-store" }
-//   );
-//   if (response.status !== 200) {
-//     return "error getCodeFromGithub: " + response.status;
-//   }
-//   const data = await response.json();
-//   return atob(data.content);
-// }
+export async function getCodeFromGithub(owner: string, repo: string, path: string) {
+  const response = await fetch(
+    "https://api.github.com/repos/" + owner + "/" + repo + "/contents/" + path,
+    // { cache: "no-store" }
+  );
+  if (response.status !== 200) {
+    return "error getCodeFromGithub: " + response.status;
+  }
+  const data = await response.json();
+  return atob(data.content);
+}
 
 export function getLocalFile(target: string) {
   target = path.join(process.cwd(), target);
