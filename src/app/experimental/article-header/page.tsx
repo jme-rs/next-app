ca4c1fe90680a93941fb193f4b9719877c31554e
jme-rs <sasaki0hajime@yahoo.co.jp>
2023-11-10 01:31:25 +0900

    Fix date prop name in article-header component

--- src/app/experimental/article-header/page.tsx
+++ src/app/experimental/article-header/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         title="記事のタイトル Title of the article"
         description="記事の説明 Description of the article"
         tags={["タグ1", "タグ2"]}
-        date="2023-10-27"
+        post="2023-10-27"
       />
 
       <h1>article-header</h1>
