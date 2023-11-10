9cac09ca54f38007e2064edb356afe6c4af3866a
jme-rs <sasaki0hajime@yahoo.co.jp>
2023-11-10 23:35:11 +0900

    Refactor post fetching to only include numbered directories.

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -2,7 +2,7 @@ import { getPosts } from "@/utils/post";
 import { Post } from "@/types/post";
 import ArticleHeader from "@/components/article-header";
 
-const posts = getPosts("src/assets/posts/**/*.md");
+const posts = getPosts("src/assets/posts/[1-9]*/*.md");
 console.log(posts);
 
 export function generateStaticParams() {
